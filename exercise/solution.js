1db059467a7232a9297157860757e7c36c7e4eaf
Александр <lion2head@gmail.com>
2016-07-18 18:32:28 +0400

    use strings.length

--- exercise/solution.js
+++ exercise/solution.js
@@ -1,15 +1,17 @@
 // BEGIN (write your solution here)
+import * as strings from './strings';
+
 const isPalindrome = (string) => {
-  if (string.length < 1) {
+  if (strings.length(string) < 1) {
     return false;
-  } else if (string.length === 1) {
+  } else if (strings.length(string) === 1) {
     return true;
   }
-  if (string.charAt(0) !== string.charAt(string.length - 1)) {
+  if (string.charAt(0) !== string.charAt(strings.length(string) - 1)) {
     return false;
   }
 
-  return isPalindrome(string.substr(1, string.length - 2));
+  return isPalindrome(string.substr(1, strings.length(string) - 2));
 };
 
 export default isPalindrome;
